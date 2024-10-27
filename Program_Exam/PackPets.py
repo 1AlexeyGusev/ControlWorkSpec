2e16f1a9b8319218f12d8a7e4327d25549c94f9a
AlexG <gusev_alexey@mail.ru>
2024-10-27 12:48:02 +0300

    1

---
+++ Program_Exam/PackPets.py
@@ -0,0 +1,18 @@
+from Animal import Animal
+from abc import ABC
+
+
+class PackPets(Animal, ABC):
+    pass
+
+
+class Horses(PackPets):
+    pass
+
+
+class Donkeys(PackPets):
+    pass
+
+
+class Camels(PackPets):
+    pass
---
+++ Program_Exam/Pets.py
@@ -0,0 +1,18 @@
+from Animal import Animal
+from abc import ABC
+
+
+class Pets(Animal, ABC):
+    pass
+
+
+class Cats(Pets):
+    pass
+
+
+class Dogs(Pets):
+    pass
+
+
+class Hamsters(Pets):
+    pass
